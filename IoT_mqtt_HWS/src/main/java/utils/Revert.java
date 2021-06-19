1eae47ecd65cd5003c73cd263200e0b70e11d074
zepnex <52677920+zepnex@users.noreply.github.com>
2021-06-19 19:45:39 +0200

    quick bugfix

--- IoT_mqtt_HWS/src/main/java/utils/Revert.java
+++ IoT_mqtt_HWS/src/main/java/utils/Revert.java
@@ -6,7 +6,7 @@ public class Revert {
 
 	public ArrayList<Message> revert(ArrayList<Message> list) {
 		ArrayList<Message> reverted = new ArrayList<>();
-		for (int i = list.size() - 1; i > 0; i--) {
+		for (int i = list.size() - 1; i >= 0; i--) {
 			// revert arraylist
 			reverted.add(list.get(i));
 			if (reverted.size() == 11)
